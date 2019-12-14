2ff9f935fd093f93304db6c22676165d69cca1eb
Joseph Hellerstein <jlheller@us.edu>
2019-12-13 17:02:23 -0800

    Update path initializations

--- examples/init.py
+++ examples/init.py
@@ -3,10 +3,10 @@ import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(
     os.path.abspath(__file__)))
+sys.path.append(PROJECT_DIR)
 
 def addSubmodule(name, project_dir=PROJECT_DIR):
   new_dir = os.path.join(project_dir, name)
   sys.path.append(new_dir)
 
-addSubmodule(PROJECT_DIR)
 addSubmodule("common_python")
